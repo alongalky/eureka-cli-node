a07f85f1fe6ece6212edc3bed6a6208834e0b1b2
Alon Gal <alongalky@gmail.com>
2017-04-15 01:36:05 +0300

    Added cost & duration to eureka tasks

--- commands/eureka-tasks.js
+++ commands/eureka-tasks.js
@@ -6,6 +6,8 @@ const tasks = require('./tasks/tasks')({
   config
 })
 const Table = require('cli-table')
+const moment = require('moment')
+const sprintf = require('sprintf-js').sprintf
 
 program
   .option('-m, --machine <type>', 'Show tasks for a specific machine')
@@ -14,12 +16,19 @@ program
 tasks.getTasks(program.machine)
   .then(response => {
     const table = new Table({
-      head: ['Task Name', 'Machine', 'Status', 'Command', 'Tier', 'Start Time']
+      head: ['Task Name', 'Machine', 'Status', 'Command', 'Tier', 'Start Time', 'Cost', 'Duration']
     })
 
     for (const task of response) {
+      let durationString = ''
+      if (task.durationInSeconds) {
+        const duration = moment.duration(task.durationInSeconds, 'seconds')
+        durationString = sprintf('%4d:%02d:%02d', Math.floor(duration.asHours()), duration.minutes(), duration.seconds())
+      }
+      const costString = task.costInCents ? sprintf('$ %6.2f', task.costInCents / 100.0) : ''
+
       table.push([task.name || '', task.machine || '', task.status || '', task.command || '',
-        task.tier || '', task.timestamp_start])
+        task.tier || '', task.timestamp_start, costString, durationString])
     }
 
     console.log(table.toString())
