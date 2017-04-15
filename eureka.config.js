7c63ff54323efa50ec50f86ea73fe8ecad9a6084
Alon Gal <alongalky@gmail.com>
2017-04-15 15:23:57 +0300

    Changed endpoint to default service

--- eureka.config.js
+++ eureka.config.js
@@ -4,7 +4,7 @@ module.exports = {
   account: '93506318-da47-499e-a6f4-c43bdc1f1eae',
 
   // DEV:
-  endpoint: 'https://eureka-api-dot-dotted-vim-164110.appspot.com'
+  endpoint: 'https://dotted-vim-164110.appspot.com'
 
   // LOCAL
   // endpoint: 'http://localhost:8080'
