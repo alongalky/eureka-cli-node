aa6a9ce3b29ed98791586f706ad9982e8a450709
Alon Gal <alongalky@gmail.com>
2017-04-20 14:02:19 +0300

    Fixed config file error format

--- config/config.js
+++ config/config.js
@@ -2,14 +2,26 @@ const os = require('os')
 const fs = require('fs')
 const path = require('path')
 const yaml = require('js-yaml')
+const chalk = require('chalk')
 
 const defaultFullPath = path.join(os.homedir(), '.eureka', 'eureka.config.yaml')
 const filename = process.env.EUREKA_CONFIG || defaultFullPath
 
-if (!fs.existsSync(filename)) {
-  throw new Error(`Missing Eureka configuration file in ${filename}. Please contact Eureka team for on-boarding instructions`)
+try {
+  if (!fs.existsSync(filename)) {
+    throw new Error(`Missing Eureka configuration file in ${filename}. Please contact Eureka team for on-boarding instructions`)
+  }
+
+  const data = yaml.safeLoad(fs.readFileSync(filename, 'utf8'))
+  module.exports = data
+} catch (err) {
+  const prefix = chalk.red('ERROR') + ` cannot read config file from ${chalk.blue(filename)}`
+  if (err.mark) {
+    // Markdown library error
+    const location = `line ${err.mark.line}, column ${err.mark.column}`
+    console.log(prefix + `. Issue with ${location}`)
+  } else {
+    console.log(prefix + `\nError message is:`, err.message)
+  }
+  process.exit()
 }
-
-const data = yaml.safeLoad(fs.readFileSync(filename, 'utf8'))
-
-module.exports = data
