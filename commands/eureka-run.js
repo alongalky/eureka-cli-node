46d974afadb1c68ded9a2d49aa045705aadd2882
Andrea Villa <andreakarimodm@gmail.com>
2017-06-30 20:14:00 +0300

    Show available tiers as part of the run help

--- commands/eureka-run.js
+++ commands/eureka-run.js
@@ -19,6 +19,25 @@ program
   })
   .option('-t, --tier [tier]', 'Run on machine of type [tier]', 'n1-standard-1')
   .option('-n, --name [name]', 'Assign a name to the task', names.getRandomName().replace(/_/g, '-'))
+
+program.on('--help', () => {
+  console.log('  Available tiers:')
+  console.log(`
+    +----------------------------------------------------------------------------------------------------+
+    | Name               | CPUs | Memory (GB) | Disk Size (GB) | SSD Disk Size | Cost per hour ($ cents) |
+    |--------------------|------|-------------|----------------|---------------|-------------------------|
+    | n1-standard-1      | 1    | 3.75        | 20             | 0             | 5.3460                  |
+    | n1-standard-2      | 2    | 7.5         | 40             | 0             | 10.6910                 |
+    | n1-standard-4      | 4    | 15          | 80             | 0             | 21.3820                 |
+    | n1-standard-4-ssd  | 4    | 15          | 0              | 375           | 33.3900                 |
+    | n1-standard-8      | 8    | 30          | 120            | 0             | 42.5240                 |
+    | n1-standard-8-ssd  | 8    | 30          | 0              | 375           | 54.2900                 |
+    | n1-standard-16-ssd | 16   | 60          | 0              | 375           | 96.0900                 |
+    | n1-standard-32-ssd | 32   | 120         | 0              | 375           | 179.6900                |
+    +----------------------------------------------------------------------------------------------------+
+    `)
+})
+
   .parse(process.argv)
 
 if (!command) {
