91acdbd25473433d70fa1403d72ba751f61e3439
Andrea Villa <andreakarimodm@gmail.com>
2017-07-12 19:55:28 +0300

    Version bump

--- commands/eureka.js
+++ commands/eureka.js
@@ -3,7 +3,7 @@
 const program = require('commander')
 
 program
-  .version('1.0.15')
+  .version('1.0.16')
   .command('run', 'Run a command')
   .command('tasks', 'Get running tasks')
   .command('kill', 'Kill a running task')
